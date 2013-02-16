bd5e9958ed63109a626512f6c11383a1fbec3cac
NK <nk@nk-G580.(none)>
2013-02-16 23:58:01 +0900

    source add

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Setlist::Application.config.secret_token = '5c44e79b3876f358f2356e694ceb1458147fb5e0d1e68e2772dd32db428e20fc6a341afb55c91ed8c073f957bf05e047ff5318ffb24e1c38d39d9edec1f4ef26'
