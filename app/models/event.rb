bc49f049d9b669e156c51d27b9d369a841be1ebc
NK <nk@nk-G580.(none)>
2013-03-02 21:14:14 +0900

    timelineのformを削除

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ActiveRecord::Base
   attr_accessible :name, :open_at, :place_id
+  belongs_to :place
+
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,4 @@
 class Place < ActiveRecord::Base
   attr_accessible :name
-    
+  has_many :events
 end
